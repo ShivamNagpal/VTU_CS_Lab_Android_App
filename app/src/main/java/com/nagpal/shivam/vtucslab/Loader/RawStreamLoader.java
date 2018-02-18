a72410c40b8078eb3d5b1d716d53da02b588b589
Shivam Nagpal <shivamnagpal432@gmail.com>
2018-02-18 16:47:25 +0530

    new feature todo added

--- app/src/main/java/com/nagpal/shivam/vtucslab/Loader/RawStreamLoader.java
+++ app/src/main/java/com/nagpal/shivam/vtucslab/Loader/RawStreamLoader.java
@@ -6,12 +6,17 @@ import android.content.Context;
 import com.nagpal.shivam.vtucslab.Utility.FetchUtil;
 
 public class RawStreamLoader extends AsyncTaskLoader<String> {
+    private static final String RAW_STREAM_LOADER = "raw_stream_loader";
     private String mUrl;
     private String fetchedData;
 
+    //    private SharedPreferences sharedPreferences;
+// TODO: Implement Local Data Saving.
     public RawStreamLoader(Context context, String url) {
         super(context);
         mUrl = url;
+//        sharedPreferences = context.getSharedPreferences(RAW_STREAM_LOADER, Context.MODE_PRIVATE);
+//        fetchedData = sharedPreferences.getString(mUrl, null);
     }
 
     @Override
@@ -26,6 +31,10 @@ public class RawStreamLoader extends AsyncTaskLoader<String> {
     @Override
     public String loadInBackground() {
         fetchedData = FetchUtil.fetchData(mUrl);
+//        TODO: If fetched data is not null.
+//        SharedPreferences.Editor editor = sharedPreferences.edit();
+//        editor.putString(mUrl, fetchedData);
+//        editor.apply();
         return fetchedData;
     }
 }
