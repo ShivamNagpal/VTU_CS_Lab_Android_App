2686b45747d51c4cb98c2a94d02f328676069c97
WallE <shivamnagpal432@gmail.com>
2023-03-17 03:33:57 +0530

    feature: Integrate ViewBinding

--- app/src/main/java/com/nagpal/shivam/vtucslab/activities/MainActivity.kt
+++ app/src/main/java/com/nagpal/shivam/vtucslab/activities/MainActivity.kt
@@ -2,14 +2,14 @@ package com.nagpal.shivam.vtucslab.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
-import com.nagpal.shivam.vtucslab.R
+import com.nagpal.shivam.vtucslab.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
-        setSupportActionBar(toolbar)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.coordinator.toolbar)
     }
 }
