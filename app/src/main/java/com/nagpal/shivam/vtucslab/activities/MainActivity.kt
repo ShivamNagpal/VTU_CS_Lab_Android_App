bba5a5187aa127125a0351fe47917bb48606d0d7
WallE <shivamnagpal432@gmail.com>
2023-03-21 03:58:55 +0530

    feature: Remove the custom Toolbar implementation and use the Default ActionBar with the Navigation Component

--- app/src/main/java/com/nagpal/shivam/vtucslab/activities/MainActivity.kt
+++ app/src/main/java/com/nagpal/shivam/vtucslab/activities/MainActivity.kt
@@ -5,44 +5,64 @@ import android.os.Handler
 import android.os.Looper
 import android.view.MenuItem
 import android.view.View
-import android.widget.ImageButton
 import androidx.activity.addCallback
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
-import androidx.drawerlayout.widget.DrawerLayout.DrawerListener
-import com.google.android.material.navigation.NavigationView
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.*
 import com.nagpal.shivam.vtucslab.R
 import com.nagpal.shivam.vtucslab.databinding.ActivityMainBinding
 import kotlin.system.exitProcess
 
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setSupportActionBar(binding.coordinator.toolbar)
-        setupDrawerToggle()
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navigation_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        setupActionBar()
         setupNavigationView()
+        setupDrawerToggle()
+    }
+
+    private fun setupActionBar() {
+        appBarConfiguration =
+            AppBarConfiguration(setOf(R.id.repositoryFragment), binding.drawerLayout)
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 
     private fun setupNavigationView() {
-        val navigationView = binding.navigationView
-        navigationView.setNavigationItemSelectedListener(this)
-        navigationView.menu.getItem(0).isChecked = true
-
-        val headerView = navigationView.getHeaderView(0)
-        val navigationDrawerBackButton =
-            headerView.findViewById<ImageButton>(R.id.close_drawer)
-        navigationDrawerBackButton.setOnClickListener { closeNavigationDrawer() }
+        binding.navigationView.setupWithNavController(navController)
+        binding.navigationView.setNavigationItemSelectedListener {
+            var flag = false
+            val itemId: Int = it.itemId
+            if (itemId == R.id.repositoryFragment) {
+                flag = true
+            } else if (itemId == R.id.menu_item_exit) {
+                exitApplication()
+                flag = true
+            }
+            if (flag) {
+                closeNavigationDrawer()
+            }
+            return@setNavigationItemSelectedListener flag
+        }
     }
 
     private fun setupDrawerToggle() {
-        val actionBarDrawerToggle = ActionBarDrawerToggle(
+        actionBarDrawerToggle = ActionBarDrawerToggle(
             this,
             binding.drawerLayout,
-            binding.coordinator.toolbar,
             R.string.drawer_open,
             R.string.drawer_close
         )
@@ -52,7 +72,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         val drawerBackPressedCallback = onBackPressedDispatcher.addCallback(this, false) {
             closeNavigationDrawer()
         }
-        binding.drawerLayout.addDrawerListener(object : DrawerListener {
+        binding.drawerLayout.addDrawerListener(object : DrawerLayout.DrawerListener {
             override fun onDrawerSlide(drawerView: View, slideOffset: Float) {
             }
 
@@ -69,27 +89,29 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         })
     }
 
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        var flag = false
-        val itemId: Int = item.itemId
-        if (itemId == R.id.menu_item_repository) {
-            flag = true
-        } else if (itemId == R.id.menu_item_exit) {
-            exitApplication()
-            flag = true
-        }
-        if (flag) {
-            closeNavigationDrawer()
-        }
-        return flag
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home && appBarConfiguration.topLevelDestinations.contains(
+                navController.currentDestination?.id
+            )
+        ) {
+            actionBarDrawerToggle.syncState()
+            if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                binding.drawerLayout.closeDrawer(GravityCompat.START)
+                return true
+            }
+        }
+        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
+    }
+
 
     private fun exitApplication() {
         finishAffinity()
         Handler(Looper.getMainLooper()).postDelayed({
-            exitProcess(
-                0
-            )
+            exitProcess(0)
         }, 1000)
     }
 
