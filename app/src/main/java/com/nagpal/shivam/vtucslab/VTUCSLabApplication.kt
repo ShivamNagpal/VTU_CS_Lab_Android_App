278a6b7045bcd13ac1423ff90dbd5ed6a7eb8329
WallE <shivamnagpal432@gmail.com>
2023-03-23 23:20:27 +0530

    feature: Disable Firebase Crashlytics for the Debug Flavor

--- app/src/main/java/com/nagpal/shivam/vtucslab/VTUCSLabApplication.kt
+++ app/src/main/java/com/nagpal/shivam/vtucslab/VTUCSLabApplication.kt
@@ -1,5 +1,13 @@
 package com.nagpal.shivam.vtucslab
 
 import androidx.multidex.MultiDexApplication
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 
-class VTUCSLabApplication : MultiDexApplication()
+class VTUCSLabApplication : MultiDexApplication() {
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            FirebaseCrashlytics.getInstance().setCrashlyticsCollectionEnabled(false)
+        }
+    }
+}
